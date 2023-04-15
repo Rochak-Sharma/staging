56d20a9c6fcfcfd6119a4bae5b81bf99f106dd7d
Rochak Sharma <rochaksharma@live.com>
2023-04-15 12:53:58 +0530

    mend

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-store = {
+store = [
+{
 
 "name" : "Raju",
 "items" : [
@@ -15,9 +16,32 @@ store = {
 ]
 
 }
+]
 
 
 
 @app.get("/store")
 def get_store():
     return {"store": store}
+
+
+@app.post("/store")
+def post_store():
+    data = request.get_json()
+
+    nameData = data['name']
+
+    store.append({"name": nameData, "items": []})
+
+    return store, 201
+
+
+
+
+@app.get("/store/<string:name>")
+def get_store_names(name):
+    for data in store:
+        if data["name"] == name:
+            return {"store": data}, 200
+    
+    return {"message": "Data wrong"}, 401
